1d96294c9384ebc084b1ffb78f973741b20f5ed0
mizukurage01 <hamudtmy755@icloud.com>
2018-11-26 07:23:02 +0900

    commit

--- python/number/combination.py
+++ python/number/combination.py
@@ -1,18 +1,18 @@
 # combination---
-# nとmは適宜変える
+# nとmodは適宜変える
 
-m = 10**9 + 7
+mod = 10**9 + 7
 fac = [1, 1]
 inv = [1, 1]
 finv = [1, 1]
 for i in range(2, n+5):
-    fac.append(fac[i-1] * i % m)
-    inv.append(m - inv[m%i] * (m//i) % m)
-    finv.append(finv[i-1] * inv[i] % m)
+    fac.append(fac[i-1] * i % mod)
+    inv.append(mod - inv[mod%i] * (mod//i) % mod)
+    finv.append(finv[i-1] * inv[i] % mod)
 
 def nck(n, k):
     if n < k:
         return 0
     if n < 0 or k < 0:
         return 0
-    return fac[n] * (finv[k] * finv[n-k] % m) % m
+    return fac[n] * (finv[k] * finv[n-k] % mod) % mod
