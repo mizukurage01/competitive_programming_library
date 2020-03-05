d39a80ce0a5e3b444362b846348b59feb7ed88f6
mizukurage01 <hamudtmy755@icloud.com>
2020-03-06 08:07:56 +0900

    create combination in c++

---
+++ c++/number/combination.cpp
@@ -0,0 +1,21 @@
+constexpr int MOD = 1e9 + 7;
+
+constexpr int MAX = 510000;
+ll fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; ++i) {
+        fac[i] = fac[i-1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD/i) % MOD;
+        finv[i] = finv[i-1] * inv[i] % MOD;
+    }
+}
+
+ll nCk(int n, int k) {
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n-k] % MOD) % MOD;
+}
--- c++/number/mod.cpp
+++ c++/number/mod.cpp
@@ -31,4 +31,14 @@ ll modpow(ll a, ll n, ll MOD) {
     return res;
 }
 
-
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
