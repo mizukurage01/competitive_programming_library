fc6b6630b9dc43a1ae5d355422dbe4ed218866f6
mizukurage01 <hamudtmy755@icloud.com>
2020-03-20 18:03:25 +0900

    fix

--- c++/string/manacher.cpp
+++ c++/string/manacher.cpp
@@ -1,6 +1,6 @@
 // reference: https://snuke.hatenablog.com/entry/2014/12/02/235837
 // ex. abaaababa -> 121412321
-// 偶数長の回文は a$b$cのようにダミー文字を間に挟むことで検出できる
+// 偶数長の回文は $a$b$c$のようにダミー文字を間に挟むことで検出できる
 // snukeさんの場合分けしたコード（下のものではない）がわかりやすい
 
 vector<int> Manacher(const string& s) {
@@ -13,5 +13,9 @@ vector<int> Manacher(const string& s) {
         while (i-k >= 0 && i+k < n && k+radius[i-k] < j) radius[i+k] = radius[i-k], ++k;
         i += k; j -= k;
     }
+    return radius;
 }
 
+
+
+// 問題例: https://codeforces.com/contest/1326/problem/D2
