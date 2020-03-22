769ceedc22146ea66f89116ea897b10f5eaedd69
Nishiguchi Tomoya <mizu@Nishiguchis-MacBook-Air.local>
2020-03-22 11:18:15 +0900

    use template

--- c++/graph/base.cpp
+++ c++/graph/base.cpp
@@ -1,5 +1,3 @@
-typedef pair<int, int> P;
-
 struct Edge{
     int to; int cost;
     Edge(int t, int c): to(t), cost(c) { }
@@ -7,27 +5,25 @@ struct Edge{
 using Graph = vector<vector<Edge>>;
 
 int MAX_V = 300;
-vector<int> dijkstra(int s, Graph& g) {
-    vector<int> dist(MAX_V, INF);
+template<typename T>
+vector<T> dijkstra(int s, Graph& g, T INF) {
+    vector<T> dist(MAX_V, INF);
     dist[s] = 0;
-    priority_queue<P, vector<P>, greater<P>> pq;
-    pq.push(P(0, s));
+    priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> pq;
+    pq.push(pair<T, int>(0, s));
     while (!pq.empty()) {
-        P q = pq.top(); pq.pop();
-        int d = q.first;
+        pair<T, int> q = pq.top(); pq.pop();
+        T d = q.first;
         int v = q.second;
         if (dist[v] < d) continue;
         for (auto nexte: g[v]) {
-            int nextd = nexte.cost;
+            T nextd = nexte.cost;
             int nextv = nexte.to;
             if (dist[v] + nextd < dist[nextv]) {
                 dist[nextv] = dist[v] + nextd;
-                pq.push(P(dist[nextv], nextv));
+                pq.push(pair<T, int>(dist[nextv], nextv));
             }
         }
     }
     return dist;
 }
-
-// Graph g(100);
-// g[0].push_back(Edge(to, cost));
